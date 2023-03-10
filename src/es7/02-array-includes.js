032c9155df01effd57a0c18c41a1c8cf5b638f07
andrescaro16 <julicaro2003@gmail.com>
2023-03-10 14:19:49 -0500

    ES7 implementations
    Every file contains a new feature of ES7

---
+++ src/es7/02-array-includes.js
@@ -0,0 +1,5 @@
+let numbers = [1, 2, 3, 4, 5, 8, 9];
+console.log(numbers.includes(7));   // El 7 está en el array? true or false
+
+const names = ["Oscar", "Andrés", "Julian"];
+console.log(names.includes("Andrés"));
